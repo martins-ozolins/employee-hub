06ea62407c8f23b62cee7cf0d7e14206312a09be
Martins Ozolins <ozolins.martinsh@gmail.com>
2026-05-01 23:06:16 +0100

    feat: add email event listener implementation

---
+++ src/main/java/com/employeehub/employeehub/event/EmailEventListener.java
@@ -0,0 +1,40 @@
+package com.employeehub.employeehub.event;
+
+import com.employeehub.employeehub.config.RabbitMqConfig;
+import com.employeehub.employeehub.service.email.EmailSender;
+import com.employeehub.employeehub.service.email.EmailTemplate;
+import com.employeehub.employeehub.service.email.templates.EmailVerificationEmail;
+import com.employeehub.employeehub.service.email.templates.PasswordChangedEmail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class EmailEventListener {
+
+    private static final Logger log = LoggerFactory.getLogger(EmailEventListener.class);
+
+    private final EmailSender emailSender;
+
+    public EmailEventListener(EmailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @RabbitListener(queues = RabbitMqConfig.EMAIL_QUEUE)
+    public void handleEmailEvent(EmailEvent event) {
+        log.info("Received email event: type={}, recipient={}", event.getType(), event.getRecipientEmail());
+
+        EmailTemplate template = switch (event.getType()) {
+            case EMAIL_VERIFICATION -> new EmailVerificationEmail(
+                    event.getData().get("baseUrl"),
+                    UUID.fromString(event.getData().get("token"))
+            );
+            case PASSWORD_CHANGED -> new PasswordChangedEmail();
+        };
+
+        emailSender.send(event.getRecipientEmail(), template);
+    }
+}
